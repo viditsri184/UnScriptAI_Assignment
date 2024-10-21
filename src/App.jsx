9f418ae57dc67aa2c4500c798e04561ae241e10f
viditsri184 <viditsrivastava760@gmail.com>
2024-10-21 22:15:47 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Headers from './components/Headers'
-import VideoComponent from './components/VideoComponent'
-import Partners from './components/Partners'
-import Feature from './components/Feature'
-import Footer from './components/Footer'
+import Landing from './pages/Landing'
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Headers />
-    <VideoComponent />
-    <Partners />
-    <Feature />
-    <Footer />
+    <Landing />
     </>
   )
 }
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Headers from './components/Headers'
+import VideoComponent from './components/VideoComponent'
+import Partners from './components/Partners'
+import Feature from './components/Feature'
+import Footer from './components/Footer'
+
+const Landing = () => {
+    return (
+        <>
+            <Navbar />
+            <Headers />
+            <VideoComponent />
+            <Partners />
+            <Feature />
+            <Footer />
+        </>
+    )
+}
+
+export default Landing
