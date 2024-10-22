7f202612d6a0f556fe846bac792d765d530b3cd2
viditsri184 <viditsrivastava760@gmail.com>
2024-10-22 21:59:12 +0530

    fixed the imports

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Headers from './components/Headers'
-import VideoComponent from './components/VideoComponent'
-import Partners from './components/Partners'
-import Feature from './components/Feature'
-import Footer from './components/Footer'
+import Navbar from '../components/Navbar'
+import Headers from '../components/Headers'
+import VideoComponent from '../components/VideoComponent'
+import Partners from '../components/Partners'
+import Feature from '../components/Feature'
+import Footer from '../components/Footer'
 
 const Landing = () => {
     return (
